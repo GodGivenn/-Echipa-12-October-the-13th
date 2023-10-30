c93315e52d68afbb5e669138b980876529360267
flavius <102308258+flvvius@users.noreply.github.com>
2023-10-30 14:54:17 +0200

    Validari pe baza de date + diverse functii de select

--- site/backend/routes/inscriere.js
+++ site/backend/routes/inscriere.js
@@ -5,4 +5,12 @@ const inscriereController = require('../controllers/inscriere');
 
 router.post('/adauga', inscriereController.adaugaInscriere);
 
+router.get('/getAll', inscriereController.getAllInscrieri);
+
+router.get('/getAllCostume', inscriereController.getAllCostume);
+
+router.get('/getAllDovleci', inscriereController.getAllDovleci);
+
+
+
 module.exports = router;
